6fbfe1fb9ab5ad42c87c2fef34f6b14629520f81
denglert <englert.david@gmail.com>
2019-05-26 20:35:30 +0100

    Adding more documentation; cleaning up framework; MCMC local test and job submission works out-of-the-box on PBS cluster.

---
+++ docs/tests.md
@@ -0,0 +1,23 @@
+# Tests
+
+After all the external packages are installed, and the scanner framework has been built, you can
+test the functionality of the scanner locally.
+
+There are some test scripts provided.
+
+## Local test of cluster job scripts
+
+
+### MCMC
+
+A local test of a MCMC jobs can be performed with the help of
+[`./job_submission/MCMC/local_test.sh`](../job_submission/MCMC/local_test.sh).
+
+This performs a local test run of T3PS chains with the output created in
+[`./job_submission/MCMC/tests/local_test/`](../job_submission/MCMC/tests/local_test/) directory.
+
+
+### SusHi
+
+
+To be filled.
--- job_submission/MCMC/local_test.sh
+++ job_submission/MCMC/local_test.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-JOB_DIR=./tests/local_test
-MCMC_CONFIG_TEMPLATE_FILE="./config/mcmc_scan.conf"
-FUNCTION_TEMPLATE="template/mcmc_scan.func"
+JOB_DIR=${THDM_T3PS_SCANNER_DIR}/job_submission/MCMC/tests/local_test
+program=${THDM_T3PS_SCANNER_DIR}/ParameterPointProcessor/bin/ParameterScan_T3PS_with_HB_HS_FAST
+MCMC_CONFIG_TEMPLATE_FILE="${THDM_T3PS_SCANNER_DIR}/job_submission/MCMC/config/mcmc_scan.conf"
+FUNCTION_TEMPLATE="${THDM_T3PS_SCANNER_DIR}/job_submission/MCMC/template/mcmc_scan.func"
 nCores=8
 chain_length=100
 
@@ -10,9 +11,10 @@ chain_length=100
 ### ----
 
 mkdir -p ${JOB_DIR}
-cp ./${MCMC_CONFIG_TEMPLATE_FILE} ${JOB_DIR}/t3ps.conf
-cp ./${FUNCTION_TEMPLATE} ${JOB_DIR}/job.template
+cp ${MCMC_CONFIG_TEMPLATE_FILE} ${JOB_DIR}/t3ps.conf
+cp ${FUNCTION_TEMPLATE} ${JOB_DIR}/job.template
 sed -i "s/nCores_/${nCores}/g" ${JOB_DIR}/t3ps.conf
+sed -i "s;program_;${program};g" ${JOB_DIR}/t3ps.conf
 sed -i "s/chain_length_/${chain_length}/g" ${JOB_DIR}/t3ps.conf
 
 echo -ne '\n\n' | t3ps --debug -o ${JOB_DIR} ${JOB_DIR}/t3ps.conf
