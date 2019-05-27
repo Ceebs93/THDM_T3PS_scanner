e0dba2eb044c656c850f7a7b83bfbf0e232b13ce
denglert <englert.david@gmail.com>
2019-05-27 13:20:00 +0100

    fixing MCMC local test poath to config  + '_default;

--- job_submission/MCMC/local_test.sh
+++ job_submission/MCMC/local_test.sh
@@ -2,7 +2,7 @@
 
 JOB_DIR=${THDM_T3PS_SCANNER_DIR}/job_submission/MCMC/tests/local_test
 program=${THDM_T3PS_SCANNER_DIR}/ParameterPointProcessor/bin/ParameterScan_T3PS_with_HB_HS_FAST
-MCMC_CONFIG_TEMPLATE_FILE="${THDM_T3PS_SCANNER_DIR}/job_submission/MCMC/config/mcmc_scan.conf"
+MCMC_CONFIG_TEMPLATE_FILE="${THDM_T3PS_SCANNER_DIR}/job_submission/MCMC/config/mcmc_scan_default.conf"
 FUNCTION_TEMPLATE="${THDM_T3PS_SCANNER_DIR}/job_submission/MCMC/template/mcmc_scan.func"
 nCores=8
 chain_length=100
