ce5c082d2fd02c8af4ad3155818e5b0ef517acdc
C.Byers <ciara.j.byers@gmail.com>
2022-02-23 18:51:59 +0000

    Removed unecessary files found inside HB/HS that will be created/downloaded automatically when those programs are set up.

---
+++ env_local.sh
@@ -0,0 +1,15 @@
+
+# Load Python environment
+conda deactivate
+module unload --all
+
+conda activate THDM
+
+# Load libraries and compiler
+module load gcc/11.1.0
+module load gsl/2.6
+
+# Load THDM and T3PS
+#echo "Path is: ${PATH}"
+echo "Scanner Dir: ${THDM_T3PS_SCANNER_DIR}"
+~                                             
