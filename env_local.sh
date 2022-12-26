5d7673b33b3d822f0a624b99ce0443252c547e41
Ciara Byers <cb27g11@soton.ac.uk>
2022-12-26 18:14:55 +0000

    Altered env_local.sh to remove references to 'modules'

--- env_local.sh
+++ env_local.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env/bash
 
 # Load Python environment
-module purge
-module load conda
-source activate THDM
+
+conda activate THDM
 
 # Load libraries and compiler
-module load gcc/11.1.0
-module load gsl/2.6
 echo "python version in use is"
 PV=$(python --version)
 echo "${PV}"
