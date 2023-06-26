c0eb66068d69e51465d2da9ef7c24b357a50e359
Ciara Byers <cb27g11@soton.ac.uk>
2023-06-26 13:27:24 +0100

    Altered env.sh, env_local.sh and setup_auto.sh from original versions for the container

--- env.sh
+++ env.sh
@@ -4,10 +4,10 @@ CWD=$(pwd)
 
 export PATH=${CWD}/packages/T3PS:$PATH
 
-export THDM_T3PS_SCANNER_DIR=/scratch/cb27g11/THDM_T3PS_scanner
+export THDM_T3PS_SCANNER_DIR=/THDM_T3PS_scanner
 echo ${THDM_T3PS_SCANNER_DIR}
 
-cd /scratch/cb27g11/THDM_T3PS_scanner
+cd /THDM_T3PS_scanner
 
 if [ -f env_local.sh ]; then
 	 echo "Found env_local.sh, sourcing it..."
--- env_local.sh
+++ env_local.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env/bash
 
 # Load Python environment
-module purge
-module load conda
 source activate THDM
 
-# Load libraries and compiler
-module load gcc/12.1.0
-module load gsl/2.7
+# Check Python version and Scanner Dir
 echo "python version in use is"
 PV=$(python --version)
 echo "${PV}"
-# Load THDM and T3PS
 echo "Scanner Dir: ${THDM_T3PS_SCANNER_DIR}"                                             
